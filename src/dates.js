abf1d65a6fa579c99bda512c68e3d2d9d4604229
marshall62 <marshall62@gmail.com>
2019-11-12 21:51:34 -0500

    Begin modifying the admin page to work in one view

---
+++ src/dates.js
@@ -0,0 +1,11 @@
+export function dateToMdy (date) {
+  var mm = date.getMonth() + 1; // getMonth() is zero-based
+  var dd = date.getDate();
+
+  var v = [
+          (mm>9 ? '' : '0') + mm,
+          (dd>9 ? '' : '0') + dd,
+          date.getFullYear()
+         ].join('/');
+  return v;  
+}
